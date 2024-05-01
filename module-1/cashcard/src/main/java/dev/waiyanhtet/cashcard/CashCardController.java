65b20dab74a897e39168c73ba3b6dd1d96be3218
wa1yan <waiyanucsy1@gmail.com>
2024-05-01 12:04:33 +0630

    added: h2 database and repository

--- module-1/cashcard/src/main/java/dev/waiyanhtet/cashcard/CashCardController.java
+++ module-1/cashcard/src/main/java/dev/waiyanhtet/cashcard/CashCardController.java
@@ -6,16 +6,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
 
+    private final CashCardRepository cashCardRepository;
+
+    private CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
     @GetMapping("/{requestedId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        if(requestedId.equals(99L)) {
-            CashCard cashCard = new CashCard(99L, 123.45);
-            return ResponseEntity.ok(cashCard);
-        }
-        return ResponseEntity.notFound().build();
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+
+        return cashCardOptional.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
---
+++ module-1/cashcard/src/main/java/dev/waiyanhtet/cashcard/CashCardRepository.java
@@ -0,0 +1,7 @@
+package dev.waiyanhtet.cashcard;
+
+import org.springframework.data.repository.CrudRepository;
+
+interface CashCardRepository extends CrudRepository<CashCard, Long> {
+
+}
